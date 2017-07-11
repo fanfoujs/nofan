974e68f1dc24cd45b4a6e5e3bddbbb6cbad072a1
LitoMore <litomore@gmail.com>
2017-07-11 23:38:41 +0800

    add 'pull request from forked' use case

--- test.js
+++ test.js
@@ -10,6 +10,8 @@ const {
   FANFOU_PASSWORD
 } = process.env
 
+const PULL_REQUEST_FROM_FORKED = !(CONSUMER_KEY && CONSUMER_SECRET && FANFOU_USERNAME && FANFOU_PASSWORD)
+
 test('nofan config', async t => {
   const {stdout} = await execa('./bin/nofan.js', ['config', CONSUMER_KEY, CONSUMER_SECRET])
   t.is(stdout, '')
@@ -17,12 +19,12 @@ test('nofan config', async t => {
 
 test('nofan login', async t => {
   const {stdout} = await execa('./bin/nofan.js', ['login', FANFOU_USERNAME, FANFOU_PASSWORD])
-  t.is(stdout, 'Login succeed!')
+  PULL_REQUEST_FROM_FORKED ? t.is(stdout, 'Invalid consumer') : t.is(stdout, 'Login succeed!')
 })
 
 test('nofan switch', async t => {
   const {stdout} = await execa('./bin/nofan.js', ['switch', FANFOU_USERNAME])
-  t.is(stdout, `Switch account to ${FANFOU_USERNAME}`)
+  PULL_REQUEST_FROM_FORKED ? t.is(stdout, `${FANFOU_USERNAME} needs login`) : t.is(stdout, `Switch account to ${FANFOU_USERNAME}`)
 })
 
 test('nofan logout', async t => {
