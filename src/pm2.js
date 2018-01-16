575a6097aba35be0b81196627509b6b6389b5069
LitoMore <litomore@gmail.com>
2018-01-16 23:17:25 +0800

    Fix restart notifier command

--- src/pm2.js
+++ src/pm2.js
@@ -38,7 +38,7 @@ const stop = async () => {
 const restart = async () => {
   await connect()
 
-  pm2.restart('nofan-notifer', err => {
+  pm2.restart('nofan-notifier', err => {
     if (err) process.spinner.fail(err.message)
     else process.spinner.succeed('Nofan Notifier restarted!')
     pm2.disconnect()
