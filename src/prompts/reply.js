a85c852b1035fdd1e85325fb64227fb01e418c96
Yocson Wang <yocson@outlook.com>
2018-07-12 07:39:33 -0400

    add line-wrap

--- src/prompts/reply.js
+++ src/prompts/reply.js
@@ -1,6 +1,19 @@
 'use strict'
+const stream = require('stream')
+const lineWrap = require('line-wrap')
+
+function wrapStatues (status) {
+  const wrapStream = lineWrap({width: 50})
+  const s = new stream.Readable()
+  s._read = () => {}
+  s.push(status.name)
+  s.push(null)
+  s.pipe(wrapStream).pipe(process.stdout)
+  return status
+}
 
 const list = statuses => {
+  statuses.map(wrapStatues)
   return [{
     type: 'list',
     name: 'status',
